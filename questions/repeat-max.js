a4e16ea798ac448c396fa98fcd67b66aa5a3a267
arslonbekXX <arslonbek.lion067@gmail.com>
2023-02-09 14:24:35 +0500

    lesson-60 start-codes

--- questions/repeat-max.js
+++ questions/repeat-max.js
@@ -1,7 +1,7 @@
 console.log("-------REPEAT MAX------");
 
 /**
- * @param {numbers[]} nums
+ * @param {number[]} nums
  * @return {number}
  */
 function repeatMax(nums) {}
@@ -9,7 +9,7 @@ function repeatMax(nums) {}
 console.log(repeatMax([1, 3, 1]) === 1);
 console.log(repeatMax([3, 3, 3, 3, 2, 2, 1]) === 3);
 console.log(repeatMax([2, 2, 1, 1, 1]) === 1);
-console.log(repeatMax([4, 3, 3, 3, 4, 4]) === 3);
+console.log(repeatMax([4, 3, 3, 3, 4, 4]) === -1);
 console.log(repeatMax([2, 3, 3]) === 3);
 console.log(repeatMax([1, 1, 1]) === 1);
 console.log(repeatMax([]) === -1);
