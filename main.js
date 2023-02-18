5762079d6cd814847c206848cc6b149a03b949d8
arslonbekXX <arslonbek.lion067@gmail.com>
2023-02-18 15:54:52 +0500

    lesson-65 finish-codes

--- main.js
+++ main.js
@@ -1,5 +1,47 @@
 "use strict";
 /* ----------- 18.02.23(65) ------------ */
-console.log("Lesson-65");
+// console.log("Lesson-65");
 
+/** __PROTO__ */
 
+// const str = "arslonbek";
+// console.log(str.__proto__);
+// console.log(str.__proto__.__proto__);
+// console.log(str.__proto__.__proto__.__proto__);
+
+/* ------REFERENCE---- */
+// const a = {
+// 	name: "arslonbek",
+// 	age: 20,
+// };
+// const b = a;
+// b.age = 60;
+// console.log(a);
+// console.log(b);
+
+/** this keyword */
+
+/**
+ * HM_0001 = {
+ *  	name: "Arslonbek",
+ *   age: 20,
+ *   run: HM_0002
+ * }
+ *
+ * HM_0002 = function () {
+ *   console.log("this = ", this)
+ * }
+ *
+ *
+ */
+const person = {
+	name: "Arslonbek",
+	age: 20,
+	run() {
+		console.log("this = ", this);
+	},
+}; // SM_person_001 = HM_0001
+
+person.run(); // HM_0001.HM_0002()
+const myRun = person.run; // SM_001_myRun = HM_0002
+myRun(); // HM_0002
