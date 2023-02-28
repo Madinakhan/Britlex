dbad284ba5a85c9248b129fbd0fb24379f5fc2d6
arslonbekXX <arslonbek.lion067@gmail.com>
2023-02-28 13:41:05 +0500

    lesson-67 start-codes

--- main.js
+++ main.js
@@ -1,173 +1,39 @@
 "use strict";
-/* ----------- 18.02.23(65) ------------ */
-// console.log("Lesson-65");
 
-/** __PROTO__ */
+/** ---------- 25.02.23 ----------(66) */
 
-// const str = "arslonbek";
-// console.log(str.__proto__);
-// console.log(str.__proto__.__proto__);
-// console.log(str.__proto__.__proto__.__proto__);
+// const boxes = document.querySelector(".boxes");
+// console.log(boxes.childNodes);
 
-/* ------REFERENCE---- */
-// const a = {
-// 	name: "arslonbek",
-// 	age: 20,
-// };
-// const b = a;
-// b.age = 60;
-// console.log(a);
-// console.log(b);
-
-/** this keyword */
-
-/**
- * HM_0001 = {
- *  	name: "Arslonbek",
- *   age: 20,
- *   run: HM_0002
- * }
- *
- * HM_0002 = function () {
- *   console.log("this = ", this)
- * }
- *
- *
- */
-// const person = {
-// 	name: "Arslonbek",
-// 	age: 20,
-// 	run() {
-// 		console.log("this = ", this);
-// 	},
-// }; // SM_person_001 = HM_0001
-
-// person.run(); // HM_0001.HM_0002()
-// const myRun = person.run; // SM_001_myRun = HM_0002
-// myRun(); // HM_0002
-
-/* ----------- 25.02.23(66) ------------ */
-// console.log("Lesson-66");
-
-// const person = {
-// 	name: "Kent",
-// 	age: 20,
-// 	run() {
-// 		console.log(this);
-// 		console.log(`${this.name} is running...`);
-// 	},
-// };
-
-// person.run();
-
-/**
- * HM_0001 = {
- *  name: "Box-1",
- *  color: "black",
- *  eat: HM_0002
- *
- * }
- *
- * HM_0002 = function () {
- *  console.log(`${this.name} is eating...`)
- * }
- */
-
-// const box = {
-// 	name: "Box-1",
-// 	color: "black",
-// 	eat() {
-// 		console.log(`${this.name} is eating...`);
-// 	},
-// }; // SM_box_001 = HM_0001
-
-// box.eat(); // HM_0001.HM_0002()
-// function run() {
-// 	console.log(`${this.name} is running...`);
-// }
-// const person = {
-// 	name: "Mark",
-// 	age: 20,
-// };
-
-// const person2 = {
-// 	name: "Jon",
-// 	age: 20,
-// };
-
-// run.call(person);
-// run.call(person2);
-
-// const person = {
-// 	name: "Kent",
-// 	birthYear: 1990,
-// 	currentYear: 2030,
-// };
-// const person1 = {
-// 	name: "Mark",
-// 	birthYear: 2010,
-// 	currentYear: 2040,
-// };
-
-// function showData() {
-// 	console.log(`${this.name} age(${this.currentYear} - ${this.birthYear}) : ${calcAge.call(this)}`);
-// 	// Kent age(2030-1990) : 40
-// }
-// function calcAge() {
-// 	return this.currentYear - this.birthYear;
+// for (let element of boxes.children) {
+// 	console.log("element: ", element);
 // }
 
-// showData.call(person);
-// showData.call(person1);
+// const first = document.getElementById("first");
 
-/**Primitive type vs Reference type */
-// HM_0001 = { value: 100 }
-// let number = { value: 100 }; // SM_number_001 = HM_0001
+// console.log(first.nextElementSibling);
+// console.log(first.nextElementSibling.nextElementSibling);
+// console.log(first.nextElementSibling.nextElementSibling.nextElementSibling);
 
-// function app(obj) {
-// 	// SM_obj_002 = HM_0001
-// 	obj.value += 100; // HM_0001.value
-// 	console.log("obj = ", obj); //  HM_0001
-// }
+const hexes = document.querySelector(".hexes");
+const addBtn = document.getElementById("add_btn");
 
-// app(number); // app(HM_0001)
-// console.log("number = ", number); // HM_0001
+function generateColor() {
+	return "#" + Math.random().toString(16).substring(2, 8);
+}
 
-/** Factory function */
-// function createBox(name, color = "White") {
-// 	return {
-// 		name,
-// 		color,
-// 		run() {
-// 			console.log(`${this.name} is running...`);
-// 		},
-// 	};
-// }
+addBtn.addEventListener("click", function () {
+	const hex = document.createElement("div");
+	hex.classList.add("hex");
+	const color = generateColor();
+	hex.innerText = color;
+	hex.style = `--color: ${color}`;
 
-// const box1 = createBox("Box-1", "Black");
-// const box2 = createBox("Box-2", "Yellow");
+	hex.addEventListener("click", function () {
+		document.body.style.backgroundColor = color;
+	});
 
-// console.log(box1);
-// console.log(box2);
+	hexes.appendChild(hex);
+});
 
-// const str = "PDP-G-7"; // new String()
-
-/**
- *  camel case : createBox
- *  pascal case : CreateBox
- */
-
-/**Constructor Function */
-
-// function Box(name, color) {
-// 	this.name = name;
-// 	this.color = color;
-// 	this.run = function () {
-// 		console.log(`${this.name} is running...`);
-// 	};
-// }
-
-// const box = new Box("Box-1", "Black");
-// box.run();
-
-// console.log(Node.DOCUMENT_NODE === document.nodeType);
+/** ---------- 28.02.23 ----------(67) */
